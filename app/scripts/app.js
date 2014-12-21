e172bd369fea4ad197f0e6307384aac80bf74f34
Umayr Shahid <umayrr@hotmail.co.uk>
2014-12-21 16:46:56 +0500

    angular app structure

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -7,16 +7,31 @@
 
     var app = angular.module('umayrshahid', ['ngRoute']);
 
-    app.config(function ($routeProvider, $locationProvider) {
+    app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
         $routeProvider
             .when('/', {
                 templateUrl: 'views/index.html',
                 controller: 'IndexController'
-            });
+            })
+            .when('/design', {
+                templateUrl: 'views/design.html',
+                controller: 'DesignController'
+            })
+            .when('/code', {
+                templateUrl: 'views/code.html',
+                controller: 'CodeController'
+            })
+            .when('/error', {
+                templateUrl: 'views/error.html',
+                controller: 'ErrorController'
+            })
+            .otherwise({
+                redirectTo: '/error'
+            })
 
-        $locationProvider.html5Mode({
+        /*$locationProvider.html5Mode({
             enabled: true,
             requireBase: false
-        });
-    });
+        });*/
+    }]);
 })();
