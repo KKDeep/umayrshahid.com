a493e15b3b4f1516003259ceab57b35f9cb3d10f
Umayr Shahid <umayrr@hotmail.co.uk>
2014-12-21 16:48:30 +0500

    angular contants

---
+++ app/scripts/constants.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Umayr on 12/20/2014.
+ */
+
+(function () {
+    'use strict';
+
+    var app = angular.module('umayrshahid');
+
+    app.constant('EVENTS', {
+        onBorderChanged: 'border-changed'
+    });
+})();
